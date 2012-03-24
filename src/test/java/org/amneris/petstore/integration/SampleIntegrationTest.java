09153b9ffd499566de27b37171e2030f0dfd0db8
Alejandro Doglioli <alejandrodoglioli@gmail.com>
2012-03-23 21:51:18 -0300

    fix the first test

--- src/test/java/org/amneris/petstore/integration/SampleIntegrationTest.java
+++ src/test/java/org/amneris/petstore/integration/SampleIntegrationTest.java
@@ -1,6 +1,15 @@
 package org.amneris.petstore.integration;
 
+import java.net.Socket;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Set;
+
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.util.WebClientUtils;
+
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.h2.server.web.WebApp;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.tynamo.test.AbstractContainerTest;
@@ -10,7 +19,12 @@ public class SampleIntegrationTest extends AbstractContainerTest
 	@Test
 	public void assertCorrectTitle() throws Exception
 	{
-		final HtmlPage homePage = webClient.getPage(BASEURI);
-		Assert.assertEquals(homePage.getTitleText(), "Tynamo!");
+		//Set the correctly used port
+		String url=BASEURI.replaceAll(":([0-9]){4}/*", ":8080/").concat("petstore");
+		final HtmlPage homePage = webClient.getPage(url);
+		Assert.assertEquals(homePage.getTitleText(), "Tinamo!");
+		
 	}
+	
+	
 }
