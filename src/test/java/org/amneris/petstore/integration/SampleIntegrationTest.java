8ee8ca77307a7da79fb656b6b9b1863c2bc529e4
Alejandro Doglioli <alejandrodoglioli@gmail.com>
2012-03-24 20:11:23 -0300

    added login test

--- src/test/java/org/amneris/petstore/integration/SampleIntegrationTest.java
+++ src/test/java/org/amneris/petstore/integration/SampleIntegrationTest.java
@@ -1,6 +1,8 @@
 package org.amneris.petstore.integration;
 
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import java.net.Socket;
@@ -12,7 +14,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 import com.gargoylesoftware.htmlunit.javascript.host.html.HTMLElement;
 import com.gargoylesoftware.htmlunit.util.WebClientUtils;
 
@@ -33,40 +39,50 @@ public class SampleIntegrationTest extends AbstractContainerTest
 {
 	private String url=PetstoreServer.BASEURL+":"+PetstoreServer.PORT;
 	
-	@BeforeMethod 
+	@BeforeClass 
 	public void setUp() throws Exception {
 		PetstoreServer.startServer();
 	}
 
-	@AfterMethod 
+	@AfterClass 
 	public void setDown() throws Exception {
 		PetstoreServer.stopServer();
 	}
-
 		 
-	@Test
-	public void assertCorrectTitleSigninPage() throws Exception
-	{		
-		final HtmlPage homePage = webClient.getPage(url);
-		Assert.assertEquals(homePage.getTitleText(), "signin");	
-	}
-	
 	@Test
 	public void assertCorrectLogin() throws Exception
 	{
 		//Set the correctly used port
-		final HtmlPage homePage = webClient.getPage(url);
+		final HtmlPage loginPage = webClient.getPage(url);
 		
-	    homePage.getElementById("jsecLogin").setNodeValue("admin");
-	    homePage.getElementById("jsecPassword").setNodeValue("admin");
+	    final HtmlForm form = loginPage.getForms().get(0);
 
+	    final HtmlTextInput userTextField = form.getInputByName("jsecLogin");
+	    final HtmlPasswordInput passwordTextField = form.getInputByName("jsecPassword");
+	    
+	    final HtmlSubmitInput button = form.getInputByName("login");
+
+		// Change the value of the text field
+		userTextField.setValueAttribute("admin");
+		passwordTextField.setValueAttribute("admin");
+		    
+		HtmlPage homePage=null;
 	    try {
-	    	homePage.getElementByName("login").click();
+	    	 homePage = button.click();
 	    } catch (Exception e){
 	    	System.out.println("Error submitting: " + e.toString());
 	    }
-	    wait(45);
-	    Assert.assertEquals(homePage.getTitleText(), "Tynamo!");	
+
+	    Assert.assertEquals(homePage.getTitleText(), "Tynamo!");
+	    webClient.closeAllWindows();
+	}
+	
+	@Test
+	public void assertCorrectTitleHomePage() throws Exception
+	{		
+		final HtmlPage homePage = webClient.getPage(url);
+		Assert.assertEquals(homePage.getTitleText(), "Tynamo!");
+		webClient.closeAllWindows();
 	}
 
 }
