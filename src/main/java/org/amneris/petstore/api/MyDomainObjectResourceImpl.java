a7ef7f21dd36c33efd6b34673bf0c0db59f8e561
Alejandro Scandroli <ascandroli@amneris.eu>
2012-04-10 23:59:18 +0200

    PersistenceService is internal and deprecated, do not use it outside the generic pages.

--- src/main/java/org/amneris/petstore/api/MyDomainObjectResourceImpl.java
+++ src/main/java/org/amneris/petstore/api/MyDomainObjectResourceImpl.java
@@ -1,7 +1,7 @@
 package org.amneris.petstore.api;
 
 import org.amneris.petstore.entities.MyDomainObject;
-import org.tynamo.services.PersistenceService;
+import org.hibernate.Session;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
@@ -10,30 +10,30 @@ import java.util.List;
 public class MyDomainObjectResourceImpl implements MyDomainObjectResource
 {
 
-	private PersistenceService persistenceService;
+	private Session session;
 
-	public MyDomainObjectResourceImpl(PersistenceService persistenceService)
+	public MyDomainObjectResourceImpl(Session session)
 	{
-		this.persistenceService = persistenceService;
+		this.session = session;
 	}
 
 	@Override
 	public List<MyDomainObject> getAllDomains()
 	{
-		return (persistenceService.getInstances(MyDomainObject.class));
+		return session.createCriteria(MyDomainObject.class).list();
 	}
 
 	@Override
 	public Response post(MyDomainObject domainObject)
 	{
-		persistenceService.save(domainObject);
+		session.save(domainObject);
 		return Response.status(Response.Status.CREATED).entity(domainObject).build();
 	}
 
 	@Override
 	public MyDomainObject getDomainObject(Long id)
 	{
-		MyDomainObject domainObject = persistenceService.getInstance(MyDomainObject.class, id);
+		MyDomainObject domainObject = (MyDomainObject) session.get(MyDomainObject.class, id);
 		if (domainObject == null)
 		{
 			throw new WebApplicationException(Response.Status.NOT_FOUND);
