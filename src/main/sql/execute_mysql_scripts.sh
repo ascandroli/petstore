a417f11859e9a93703022bdeb3ab618be7ca7f95
ilopez <ignasi.lopez.luna@gmail.com>
2012-04-29 18:36:13 +0200

    Deleted use of DBDeploy. Instead we're going to try using Flyway.
    It solves:
    - Dbdeploy doesn't support delimiters for stored procedures
    - Script for changelog table
    - Build.xml doesn't needed anymore
    - Migrations profile doesn't needed anymore.

--- src/main/sql/execute_mysql_scripts.sh
+++
@@ -1,5 +0,0 @@
-mysql -u root -proot < create_databases.sql
-mysql petstore -u root -proot < create_changelog_table.sql
-mysql petstore_dev -u root -proot < create_changelog_table.sql
-mysql petstore_tests -u root -proot < create_changelog_table.sql
-
