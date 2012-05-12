cb9e0b58073e30755eb828b1fc7826b7331bb4c1
ilopez <ignasi.lopez.luna@gmail.com>
2012-05-12 19:04:33 +0200

    Update delta for Flyway

--- src/main/resources/db/migration/V1_Initial_version.sql
+++ src/main/resources/db/migration/V1_Initial_version.sql
@@ -1,377 +1,52 @@
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
---
--- Base de datos: `petstore_dev`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `carts`
---
-
-CREATE TABLE IF NOT EXISTS `carts` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `created_on` datetime DEFAULT NULL,
-  `updated_on` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Volcar la base de datos para la tabla `carts`
---
-
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cart_x_products`
---
-
-CREATE TABLE IF NOT EXISTS `cart_x_products` (
-  `quantity` int(11) NOT NULL,
-  `product_id` bigint(20) NOT NULL,
-  `cart_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`product_id`,`cart_id`),
-  KEY `FK9497AFCAF24D489A` (`cart_id`),
-  KEY `FK9497AFCA9B95007A` (`product_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcar la base de datos para la tabla `cart_x_products`
---
-
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `categories`
---
-
-CREATE TABLE IF NOT EXISTS `categories` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `active` bit(1) NOT NULL,
-  `created_on` datetime DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `link_rewrite` varchar(255) DEFAULT NULL,
-  `meta_description` varchar(255) DEFAULT NULL,
-  `meta_keywords` varchar(255) DEFAULT NULL,
-  `meta_title` varchar(255) DEFAULT NULL,
-  `name` varchar(255) NOT NULL,
-  `updated_on` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Volcar la base de datos para la tabla `categories`
---
-
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `changelog`
---
-
-CREATE TABLE IF NOT EXISTS `changelog` (
-  `CHANGE_NUMBER` bigint(20) NOT NULL,
-  `COMPLETE_DT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `APPLIED_BY` varchar(100) NOT NULL,
-  `DESCRIPTION` varchar(500) NOT NULL,
-  PRIMARY KEY (`CHANGE_NUMBER`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcar la base de datos para la tabla `changelog`
---
-
-INSERT INTO `changelog` (`CHANGE_NUMBER`, `COMPLETE_DT`, `APPLIED_BY`, `DESCRIPTION`) VALUES
-(1, '2012-04-12 20:42:34', 'root@localhost', '1.sql');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `currencies`
---
-
-CREATE TABLE IF NOT EXISTS `currencies` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `active` bit(1) NOT NULL,
-  `conversion_rate` double DEFAULT NULL,
-  `created_on` datetime DEFAULT NULL,
-  `iso_code` varchar(255) DEFAULT NULL,
-  `name` varchar(255) NOT NULL,
-  `updated_on` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Volcar la base de datos para la tabla `currencies`
---
-
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `customers`
---
-
-CREATE TABLE IF NOT EXISTS `customers` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `active` bit(1) NOT NULL,
-  `birthday` datetime DEFAULT NULL,
-  `created_on` datetime DEFAULT NULL,
-  `email` varchar(255) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `newsletter` bit(1) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `updated_on` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `first_name` (`first_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Volcar la base de datos para la tabla `customers`
---
-
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `employes`
---
-
-CREATE TABLE IF NOT EXISTS `employes` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `active` bit(1) NOT NULL,
-  `created_on` datetime DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `updated_on` datetime DEFAULT NULL,
-  `profile_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `first_name` (`first_name`),
-  KEY `FK4722E6BCF1600E3A` (`profile_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Volcar la base de datos para la tabla `employes`
---
-
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `manufacturers`
---
-
-CREATE TABLE IF NOT EXISTS `manufacturers` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `active` bit(1) NOT NULL,
-  `created_on` datetime DEFAULT NULL,
-  `link_rewrite` varchar(255) DEFAULT NULL,
-  `meta_description` varchar(255) DEFAULT NULL,
-  `meta_keywords` varchar(255) DEFAULT NULL,
-  `meta_title` varchar(255) DEFAULT NULL,
-  `name` varchar(255) NOT NULL,
-  `updated_on` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Volcar la base de datos para la tabla `manufacturers`
---
-
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `mydomainobjects`
---
-
-CREATE TABLE IF NOT EXISTS `mydomainobjects` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `checked` bit(1) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Volcar la base de datos para la tabla `mydomainobjects`
---
-
-INSERT INTO `mydomainobjects` (`id`, `checked`, `name`) VALUES
-(1, b'0', 'dummy');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `orders`
---
-
-CREATE TABLE IF NOT EXISTS `orders` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `created_on` datetime DEFAULT NULL,
-  `delivery_date` datetime DEFAULT NULL,
-  `payment` varchar(255) NOT NULL,
-  `shipping_number` varchar(255) NOT NULL,
-  `total_discount` double DEFAULT NULL,
-  `total_paid` double DEFAULT NULL,
-  `total_producs` int(11) DEFAULT NULL,
-  `updated_on` datetime DEFAULT NULL,
-  `cart_id` bigint(20) DEFAULT NULL,
-  `currency_id` bigint(20) DEFAULT NULL,
-  `customer_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FKC3DF62E5B105E6FA` (`currency_id`),
-  KEY `FKC3DF62E5F24D489A` (`cart_id`),
-  KEY `FKC3DF62E52644B7DA` (`customer_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Volcar la base de datos para la tabla `orders`
---
-
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `products`
---
-
-CREATE TABLE IF NOT EXISTS `products` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `active` bit(1) NOT NULL,
-  `created_on` datetime DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `link_rewrite` varchar(255) DEFAULT NULL,
-  `meta_description` varchar(255) DEFAULT NULL,
-  `meta_keywords` varchar(255) DEFAULT NULL,
-  `meta_title` varchar(255) DEFAULT NULL,
-  `name` varchar(255) NOT NULL,
-  `price` bigint(20) DEFAULT NULL,
-  `quantity` int(11) NOT NULL,
-  `reduction_price` bigint(20) DEFAULT NULL,
-  `updated_on` datetime DEFAULT NULL,
-  `category_id` bigint(20) DEFAULT NULL,
-  `manufacturer_id` bigint(20) DEFAULT NULL,
-  `supplier_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`),
-  KEY `FKC42BD164CA6E23DA` (`category_id`),
-  KEY `FKC42BD164A7B3DEFA` (`manufacturer_id`),
-  KEY `FKC42BD164EB72491A` (`supplier_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Volcar la base de datos para la tabla `products`
---
-
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `profiles`
---
-
-CREATE TABLE IF NOT EXISTS `profiles` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `active` bit(1) NOT NULL,
-  `created_on` datetime DEFAULT NULL,
-  `name` varchar(255) NOT NULL,
-  `updated_on` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Volcar la base de datos para la tabla `profiles`
---
-
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `suppliers`
---
-
-CREATE TABLE IF NOT EXISTS `suppliers` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `active` bit(1) NOT NULL,
-  `created_on` datetime DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `link_rewrite` varchar(255) DEFAULT NULL,
-  `meta_description` varchar(255) DEFAULT NULL,
-  `meta_keywords` varchar(255) DEFAULT NULL,
-  `meta_title` varchar(255) DEFAULT NULL,
-  `name` varchar(255) NOT NULL,
-  `updated_on` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Volcar la base de datos para la tabla `suppliers`
---
-
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tabs`
---
-
-CREATE TABLE IF NOT EXISTS `tabs` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `active` bit(1) NOT NULL,
-  `created_on` datetime DEFAULT NULL,
-  `name` varchar(255) NOT NULL,
-  `position` int(11) NOT NULL,
-  `updated_on` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Volcar la base de datos para la tabla `tabs`
---
-
-
---
--- Filtros para las tablas descargadas (dump)
---
-
---
--- Filtros para la tabla `cart_x_products`
---
-ALTER TABLE `cart_x_products`
-  ADD CONSTRAINT `FK9497AFCA9B95007A` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
-  ADD CONSTRAINT `FK9497AFCAF24D489A` FOREIGN KEY (`cart_id`) REFERENCES `carts` (`id`);
-
---
--- Filtros para la tabla `employes`
---
-ALTER TABLE `employes`
-  ADD CONSTRAINT `FK4722E6BCF1600E3A` FOREIGN KEY (`profile_id`) REFERENCES `profiles` (`id`);
-
---
--- Filtros para la tabla `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `FKC3DF62E52644B7DA` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`),
-  ADD CONSTRAINT `FKC3DF62E5B105E6FA` FOREIGN KEY (`currency_id`) REFERENCES `currencies` (`id`),
-  ADD CONSTRAINT `FKC3DF62E5F24D489A` FOREIGN KEY (`cart_id`) REFERENCES `carts` (`id`);
-
---
--- Filtros para la tabla `products`
---
-ALTER TABLE `products`
-  ADD CONSTRAINT `FKC42BD164EB72491A` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`),
-  ADD CONSTRAINT `FKC42BD164A7B3DEFA` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturers` (`id`),
-  ADD CONSTRAINT `FKC42BD164CA6E23DA` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
+--alter table accesses drop foreign key FK800FF91287E4C6BA;
+--alter table accesses drop foreign key FK800FF912F1600E3A;
+--alter table cart_x_products drop foreign key FK9497AFCAF24D489A;
+--alter table cart_x_products drop foreign key FK9497AFCA9B95007A;
+--alter table carts drop foreign key FK5A0E9532644B7DA;
+--alter table employes drop foreign key FK4722E6BCF1600E3A;
+--alter table orders drop foreign key FKC3DF62E5B105E6FA;
+--alter table orders drop foreign key FKC3DF62E5F24D489A;
+--alter table orders drop foreign key FKC3DF62E52644B7DA;
+--alter table products drop foreign key FKC42BD164CA6E23DA;
+--alter table products drop foreign key FKC42BD164A7B3DEFA;
+--alter table products drop foreign key FKC42BD164EB72491A;
+drop table if exists accesses;
+drop table if exists cart_x_products;
+drop table if exists carts;
+drop table if exists categories;
+drop table if exists currencies;
+drop table if exists customers;
+drop table if exists employes;
+drop table if exists manufacturers;
+drop table if exists mydomainobjects;
+drop table if exists orders;
+drop table if exists products;
+drop table if exists profiles;
+drop table if exists suppliers;
+drop table if exists tabs;
+create table accesses (id bigint not null auto_increment, add_ integer, delete_ integer, edit_ integer, view_ integer, profile_id bigint, tab_id bigint, primary key (id)) ENGINE=InnoDB;
+create table cart_x_products (id bigint not null auto_increment, quantity integer not null, cart_id bigint, product_id bigint, primary key (id)) ENGINE=InnoDB;
+create table carts (id bigint not null auto_increment, created_on datetime, updated_on datetime, customer_id bigint, primary key (id)) ENGINE=InnoDB;
+create table categories (id bigint not null auto_increment, active bit not null, created_on datetime, description varchar(255), link_rewrite varchar(255), meta_description varchar(255), meta_keywords varchar(255), meta_title varchar(255), name varchar(255), updated_on datetime, primary key (id), unique (name)) ENGINE=InnoDB;
+create table currencies (id bigint not null auto_increment, active bit not null, conversion_rate double precision, created_on datetime, iso_code varchar(255), name varchar(255), updated_on datetime, primary key (id), unique (name)) ENGINE=InnoDB;
+create table customers (id bigint not null auto_increment, active bit not null, birthday datetime, created_on datetime, email varchar(255), first_name varchar(255), last_name varchar(255), newsletter bit not null, password varchar(255), updated_on datetime, primary key (id)) ENGINE=InnoDB;
+create table employes (id bigint not null auto_increment, active bit not null, created_on datetime, email varchar(255), first_name varchar(255), last_name varchar(255), password varchar(255), updated_on datetime, profile_id bigint, primary key (id), unique (first_name)) ENGINE=InnoDB;
+create table manufacturers (id bigint not null auto_increment, active bit not null, created_on datetime, link_rewrite varchar(255), meta_description varchar(255), meta_keywords varchar(255), meta_title varchar(255), name varchar(255), updated_on datetime, primary key (id), unique (name)) ENGINE=InnoDB;
+create table mydomainobjects (id bigint not null auto_increment, checked bit not null, name varchar(255), primary key (id)) ENGINE=InnoDB;
+create table orders (id bigint not null auto_increment, created_on datetime, delivery_date datetime, payment varchar(255), shipping_number varchar(255), total_discount double precision, total_paid double precision, total_producs integer, updated_on datetime, cart_id bigint, currency_id bigint, customer_id bigint, primary key (id)) ENGINE=InnoDB;
+create table products (id bigint not null auto_increment, active bit not null, created_on datetime, description varchar(255), link_rewrite varchar(255), meta_description varchar(255), meta_keywords varchar(255), meta_title varchar(255), name varchar(255), price bigint, quantity integer not null, reduction_price bigint, updated_on datetime, category_id bigint, manufacturer_id bigint, supplier_id bigint, primary key (id), unique (name)) ENGINE=InnoDB;
+create table profiles (id bigint not null auto_increment, active bit not null, created_on datetime, name varchar(255), updated_on datetime, primary key (id), unique (name)) ENGINE=InnoDB;
+create table suppliers (id bigint not null auto_increment, active bit not null, created_on datetime, description varchar(255), link_rewrite varchar(255), meta_description varchar(255), meta_keywords varchar(255), meta_title varchar(255), name varchar(255), updated_on datetime, primary key (id), unique (name)) ENGINE=InnoDB;
+create table tabs (id bigint not null auto_increment, active bit not null, created_on datetime, name varchar(255), position integer not null, updated_on datetime, primary key (id), unique (name)) ENGINE=InnoDB;
+alter table accesses add index FK800FF91287E4C6BA (tab_id), add constraint FK800FF91287E4C6BA foreign key (tab_id) references tabs (id);
+alter table accesses add index FK800FF912F1600E3A (profile_id), add constraint FK800FF912F1600E3A foreign key (profile_id) references profiles (id);
+alter table cart_x_products add index FK9497AFCAF24D489A (cart_id), add constraint FK9497AFCAF24D489A foreign key (cart_id) references carts (id);
+alter table cart_x_products add index FK9497AFCA9B95007A (product_id), add constraint FK9497AFCA9B95007A foreign key (product_id) references products (id);
+alter table carts add index FK5A0E9532644B7DA (customer_id), add constraint FK5A0E9532644B7DA foreign key (customer_id) references customers (id);
+alter table employes add index FK4722E6BCF1600E3A (profile_id), add constraint FK4722E6BCF1600E3A foreign key (profile_id) references profiles (id);
+alter table orders add index FKC3DF62E5B105E6FA (currency_id), add constraint FKC3DF62E5B105E6FA foreign key (currency_id) references currencies (id);
+alter table orders add index FKC3DF62E5F24D489A (cart_id), add constraint FKC3DF62E5F24D489A foreign key (cart_id) references carts (id);
+alter table orders add index FKC3DF62E52644B7DA (customer_id), add constraint FKC3DF62E52644B7DA foreign key (customer_id) references customers (id);
+alter table products add index FKC42BD164CA6E23DA (category_id), add constraint FKC42BD164CA6E23DA foreign key (category_id) references categories (id);
+alter table products add index FKC42BD164A7B3DEFA (manufacturer_id), add constraint FKC42BD164A7B3DEFA foreign key (manufacturer_id) references manufacturers (id);
+alter table products add index FKC42BD164EB72491A (supplier_id), add constraint FKC42BD164EB72491A foreign key (supplier_id) references suppliers (id);
